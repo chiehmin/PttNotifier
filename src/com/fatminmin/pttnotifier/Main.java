fca0af28dae626254d89ff3874305eb98a29f63e
FatMinMin <chiehmin18@gmail.com>
2016-06-28 23:14:32 +0800

    initial gui

--- src/com/fatminmin/pttnotifier/Main.java
+++ src/com/fatminmin/pttnotifier/Main.java
@@ -1,10 +1,27 @@
 package com.fatminmin.pttnotifier;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by fatminmin on 6/28/16.
  */
-public class Main {
-    public static void main(String[] args) {
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
---
+++ src/com/fatminmin/pttnotifier/UIController.java
@@ -0,0 +1,7 @@
+package com.fatminmin.pttnotifier;
+
+/**
+ * Created by fatminmin on 6/28/16.
+ */
+public class UIController {
+}
